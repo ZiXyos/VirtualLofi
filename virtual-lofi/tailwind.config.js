5e8bf588bf047501ee9ca1841ee29095743aab71
ZiXyos <killian.moudery@epitech.eu>
2021-06-02 18:49:58 +0200

    [Add]: tailwind

---
+++ virtual-lofi/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./pages/**/*.jsx', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+}
